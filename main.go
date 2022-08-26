84c945ce26344fceb364434ae21283cbdcbd6669
Tarjei Utnes <tutnes@gmail.com>
2022-08-26 15:21:33 +0200

    Updated

--- main.go
+++ main.go
@@ -1,130 +1,60 @@
-// Copyright The OpenTelemetry Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
-	"context"
-	"io"
-	"log"
-	"os"
-	"os/signal"
+	"fmt"
 
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
-	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
-	"go.opentelemetry.io/otel/propagation"
-	"go.opentelemetry.io/otel/sdk/resource"
-	"go.opentelemetry.io/otel/sdk/trace"
-
-	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/valyala/fastjson"
 )
 
-func InitTracerProvider() *sdktrace.TracerProvider {
-	ctx := context.Background()
+func HandleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	ApiResponse := events.APIGatewayProxyResponse{}
+	// Switch for identifying the HTTP request
+	switch request.HTTPMethod {
+	case "GET":
+		// Obtain the QueryStringParameter
+		name := request.QueryStringParameters["name"]
+		if name != "" {
+			ApiResponse = events.APIGatewayProxyResponse{Body: "Hey " + name + " welcome! ", StatusCode: 200}
+		} else {
+			ApiResponse = events.APIGatewayProxyResponse{Body: "Error: Query Parameter name missing", StatusCode: 500}
+		}
 
-	res, err := resource.New(ctx,
-		resource.WithAttributes(
-			semconv.ServiceNameKey.String("go-quickstart"), //TODO Replace with the name of your application
-			semconv.ServiceVersionKey.String("1.0.1"),      //TODO Replace with the version of your application
-		),
-	)
-	if err != nil {
-		log.Fatalf("Failed to create resource: %v", err)
+	case "POST":
+		//validates json and returns error if not working
+		err := fastjson.Validate(request.Body)
+
+		if err != nil {
+			body := "Error: Invalid JSON payload ||| " + fmt.Sprint(err) + " Body Obtained" + "||||" + request.Body
+			ApiResponse = events.APIGatewayProxyResponse{Body: body, StatusCode: 500}
+		} else {
+			ApiResponse = events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}
+		}
+
 	}
-
-	exporter, err := otlptracehttp.New(
-		ctx,
-		otlptracehttp.WithEndpoint("dtmantest.sa.evry.com"),
-		otlptracehttp.WithURLPath("/e/26096a42-7e67-44f7-bf63-3fe4bba7958c/api/v2/otlp/v1/traces"), //TODO Replace <URL_PATH> to your SaaS/Managed-URL-PATH as mentioned in the next step
-		otlptracehttp.WithHeaders(map[string]string{"Authorization": "Api-Token "}),                //TODO Replace <TOKEN> with your API Token as mentioned in the next step
-	)
-	if err != nil {
-		log.Fatalf("Failed to create OTLP exporter: %v", err)
-	}
-
-	tp := sdktrace.NewTracerProvider(
-		sdktrace.WithResource(res),
-		sdktrace.WithSampler(sdktrace.AlwaysSample()),
-		sdktrace.WithBatcher(exporter),
-	)
-	otel.SetTracerProvider(tp)
-	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
-	return tp
+	// Response
+	return ApiResponse, nil
 }
 
-// newExporter returns a console exporter.
-func newExporter(w io.Writer) (trace.SpanExporter, error) {
-	return stdouttrace.New(
-		stdouttrace.WithWriter(w),
-		// Use human readable output.
-		stdouttrace.WithPrettyPrint(),
-		// Do not print timestamps for the demo.
-		stdouttrace.WithoutTimestamps(),
-	)
-}
+// Fibonacci returns the n-th fibonacci number.
+func Fibonacci(n uint) (uint64, error) {
+	if n <= 1 {
+		return uint64(n), nil
+	}
 
-// newResource returns a resource describing this application.
-func newResource() *resource.Resource {
-	r, _ := resource.Merge(
-		resource.Default(),
-		resource.NewWithAttributes(
-			semconv.SchemaURL,
-			semconv.ServiceNameKey.String("fib"),
-			semconv.ServiceVersionKey.String("v0.1.0"),
-			attribute.String("environment", "demo"),
-		),
-	)
-	return r
+	if n > 93 {
+		return 0, fmt.Errorf("unsupported fibonacci number %d: too large", n)
+	}
+
+	var n2, n1 uint64 = 0, 1
+	for i := uint(2); i < n; i++ {
+		n2, n1 = n1, n1+n2
+	}
+
+	return n2 + n1, nil
 }
 
 func main() {
-	otel.SetTracerProvider(sdktrace.NewTracerProvider())
-	l := log.New(os.Stdout, "", 0)
-	tp := InitTracerProvider()
-	defer func() {
-		if err := tp.Shutdown(context.Background()); err != nil {
-			log.Printf("Error shutting down tracer provider: %v", err)
-		}
-	}()
-	// Write telemetry data to a file.
-	f, err := os.Create("traces.txt")
-	if err != nil {
-		l.Fatal(err)
-	}
-	defer f.Close()
-
-	otel.SetTracerProvider(tp)
-
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, os.Interrupt)
-
-	errCh := make(chan error)
-	app := NewApp(os.Stdin, l)
-	go func() {
-		errCh <- app.Run(context.Background())
-	}()
-
-	select {
-	case <-sigCh:
-		l.Println("\ngoodbye")
-		return
-	case err := <-errCh:
-		if err != nil {
-			l.Fatal(err)
-		}
-	}
+	lambda.Start(HandleRequest)
 }
